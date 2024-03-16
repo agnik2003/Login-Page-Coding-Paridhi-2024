a53a73f7a1726c19f9f9e7d28c336fd1fc612909
Agnik Mondal <132749193+agnik2003@users.noreply.github.com>
2024-03-16 21:02:14 +0530

    Update signup.js

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 import {
   AltLogin,
   AltLoginButton,
@@ -32,13 +34,33 @@ import { gapi } from "gapi-script";
 
 const SignUp = () => {
   const [inputList, setinputList] = useState([{ name: "" }]);
+  const [verificationResults, setVerificationResults] = useState([]);
 
   const handleinputchange = (e, index) => {
     const { name, value } = e.target;
     const list = [...inputList];
     list[index][name] = value;
     setinputList(list);
+    // verifyGID(value);
   };
+
+  // const verifyGID = async (value) => {
+  //   try {
+  //     const response = await axios.get(`http://localhost:6001/paridhi/event/coding/allyearcp/${value}`);
+  //     if (response.status === 200) {
+  //       // GID is valid, handle accordingly
+  //       console.log(`GID ${value} is valid`);
+  //     } else {
+  //       // GID is invalid, handle accordingly
+  //       console.log(`GID ${value} is invalid`);
+  //     }
+  //   } catch (error) {
+  //     // Error occurred while verifying GID, handle accordingly
+  //     console.error("Error verifying GID:", error);
+  //   }
+  // };
+
+
 
   const handleremove=index=>{
     const list=[...inputList];
@@ -51,10 +73,36 @@ const SignUp = () => {
   const handleaddclick = () => {
     if (inputList.length < 2) {
       setinputList([...inputList, { name: "" }]);
+      setVerificationResults([...verificationResults, null]);
     } else {
       alert("You can only add up to 2 names");
     }
   };
+
+  const verifyGID = async (value, index) => {
+    try {
+      const response = await axios.get(`http://localhost:6001/paridhi/event/coding/allyearcp/${value}`);
+      if (response.status === 200) {
+        setVerificationResults(prevResults => {
+          const updatedResults = [...prevResults];
+          updatedResults[index] = "valid";
+          return updatedResults;
+        });
+        console.log(`GID ${value} is valid`);
+      } else {
+        setVerificationResults(prevResults => {
+          const updatedResults = [...prevResults];
+          updatedResults[index] = "invalid";
+          return updatedResults;
+        });
+        console.log(`GID ${value} is invalid`);
+      }
+    } catch (error) {
+      console.error("Error verifying GID:", error);
+    }
+  };
+
+
   useEffect(() => {
     function start() {
       gapi.client.init({
@@ -99,21 +147,31 @@ const SignUp = () => {
 
           {inputList.map((x, i) => {
         return (
-          <IconContainer>
+          <IconContainer key={i}>
             <InputIcon className="fa fa-id-card-o" aria-hidden="true"></InputIcon>
-            <InputField name="name" type="number" placeholder={`GID ${i+1}`} onChange={(e) => handleinputchange(e, i)} />
+            <InputField name={`gid ${i+1}`} type="text" placeholder={`GID ${i+1}`} onChange={(e) => handleinputchange(e, i)} />
             {inputList.length !== 1 && (
             <Button className="fa fa-minus" onClick={() => handleremove(i)}></Button>
             )}
             {inputList.length - 1 === i && (
               <Button className="fa fa-plus"  onClick={handleaddclick}></Button>
             )}
-            <Button className="Verify"> Verify</Button>
+            <Button className="Verify" onClick={() => verifyGID(x.name, i)}> Verify</Button>
+            {verificationResults[i] && <Text>{verificationResults[i]}</Text>}
             
 
           </IconContainer>
           );
         })}
+           {/* <IconContainer>
+            <InputIcon
+              className="fa fa-id-card-o"
+              ar5ia-hidden="true"
+            ></InputIcon>
+            <InputField name="college" type="number" placeholder="TID" />
+            <Button className="Verify"> Verify</Button>
+
+          </IconContainer> */}
 
           <SignUpButton onClick={popup}>Pay Now</SignUpButton>
         </Container>
